484b0db61406e58d5a1ffb37f8007bf349c55f88
swish <chinawsb@sina.com>
2023-11-30 15:34:54 +0800

    + 增加身份证和手机号两个类型的资源字符串

--- source/qdac.resource.pas
+++ source/qdac.resource.pas
@@ -8,6 +8,8 @@ resourcestring
   SLengthOnlySupportStringAndArray = '长度验证只支持字符串和数组类型';
   SAssertSizeError = 'AMaxSize 必需 >= AMinSize 并表 AMinSize must >= 0';
   SValueTypeError = '[Value] 不是一个有效的[ValueType]值';
+  SChineseMobile = '中国大陆手机号码';
+  SChineseId = '中国大陆居民身份证号';
 implementation
 
 end.
