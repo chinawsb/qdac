29fe42ce80144db672b25d7d1cbc04700e9ef3b9
swish <chinawsb@sina.com>
2023-11-30 11:24:30 +0800

    + 资源字符串所在文件，默认中文，如果要本地化，翻译成对应的语言替换即可

---
+++ source/qdac.resource.pas
@@ -0,0 +1,10 @@
+unit qdac.resource;
+
+interface
+
+resourcestring
+  SDefaultRangeError = '[Value] 不在 [MinValue] 和 [MaxValue] 之间，取值无效.';
+  SDefaultLengthError = '长度 [Size] 不在 [MinSize] 和 [MaxSize] 之间，取值无效.';
+implementation
+
+end.
