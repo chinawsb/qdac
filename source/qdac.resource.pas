0a4945b99449c612d55010c00fee1fc22b9a9a74
swish <chinawsb@sina.com>
2023-11-30 15:27:27 +0800

    + 增加验证使用的多个资源字符串

--- source/qdac.resource.pas
+++ source/qdac.resource.pas
@@ -5,6 +5,9 @@ interface
 resourcestring
   SDefaultRangeError = '[Value] 不在 [MinValue] 和 [MaxValue] 之间，取值无效.';
   SDefaultLengthError = '长度 [Size] 不在 [MinSize] 和 [MaxSize] 之间，取值无效.';
+  SLengthOnlySupportStringAndArray = '长度验证只支持字符串和数组类型';
+  SAssertSizeError = 'AMaxSize 必需 >= AMinSize 并表 AMinSize must >= 0';
+  SValueTypeError = '[Value] 不是一个有效的[ValueType]值';
 implementation
 
 end.
