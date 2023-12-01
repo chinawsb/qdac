31a06ff4649f433703a712277b565e22d59b5810
swish <chinawsb@sina.com>
2023-12-01 13:50:26 +0800

    增加 qdac.common 单元，并将长度类型设置为 SizeInt

---
+++ source/qdac.common.pas
@@ -0,0 +1,19 @@
+﻿unit qdac.common;
+
+interface
+uses System.Classes,System.SysUtils,System.Generics.Collections;
+
+{$I qdac.inc}
+type
+  SizeInt=NativeUInt;
+  TUnicodeStringArray=TArray<UnicodeString>;
+
+const
+  {$IFDEF NEXTGEN}
+  STRING_FIRST_INDEX=0;
+  {$ELSE}
+  STRING_FIRST_INDEX=1;
+  {$ENDIF}
+implementation
+
+end.
