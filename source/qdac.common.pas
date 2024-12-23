1a1d60707871a1ad43bdcc6a9c3e6bf47958129f
swish <109867294@qq.com>
2024-12-23 18:05:53 +0800

    + 增加 TStringPair 对应 TPair<String,String> 的定义

--- source/qdac.common.pas
+++ source/qdac.common.pas
@@ -8,7 +8,7 @@ type
   SizeInt=NativeUInt;
   TUnicodeStringArray=TArray<UnicodeString>;
   TStringArray=TUnicodeStringArray;
-
+  TStringPair=TPair<String,String>;
 const
   //NEXTGEN是XE3加入的编译器选项，10.4Sydney开始NEXTGEN已被移除
   {$IFDEF NEXTGEN}
