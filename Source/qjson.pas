f45cc962692ef63c42641ef0fc505c93d065bcfb
swish <109867294@qq.com>
2025-09-12 11:56:50 +0800

    + Add Stream codec support

--- Source/qjson.pas
+++ Source/qjson.pas
@@ -578,7 +578,46 @@ type
     constructor Create(AJson: TQJson); overload;
     destructor Destroy; override;
   end;
+  IQTextCodec = interface
+    ['{87FCBDD7-3678-4FD6-B0FA-3D8596D36CDD}']
+    function Encode: UnicodeString;
+    procedure Decode(const AValue: UnicodeString);
+  end;
 
+  IQStreamCodec = interface
+    ['{8D7435A8-BF6F-4D5C-918D-9CB880B559A5}']
+    procedure LoadFromBuffer(const p: PByte; ACount: UInt64); overload;
+    procedure LoadFromBuffer(const ABytes: TBytes; const AOffset, ACount: UInt64); overload;
+    procedure LoadFromFile(const AFileName: UnicodeString; AMode: Word);
+    function GetAsStream: TStream;
+    property AsStream: TStream read GetAsStream;
+  end;
+
+  TStreamCodecClass = class of TBaseStreamCodec;
+
+  TBaseStreamCodec = class(TInterfacedObject, IQStreamCodec)
+  private
+    FStream: TStream;
+    procedure LoadFromBuffer(const p: PByte; ACount: UInt64); overload;
+    procedure LoadFromBuffer(const ABytes: TBytes; const AOffset, ACount: UInt64); overload;
+    procedure LoadFromFile(const AFileName: UnicodeString; AMode: Word);
+    function GetAsStream: TStream;
+  public
+    constructor Create; overload;
+    destructor Destroy; override;
+  end;
+
+  TBaseStreamTextCodec = class(TBaseStreamCodec, IQTextCodec)
+  protected
+    function Encode: UnicodeString; virtual; abstract;
+    procedure Decode(const AValue: UnicodeString); virtual; abstract;
+  end;
+
+  TBase64StreamCodec = class(TBaseStreamTextCodec)
+  protected
+    function Encode: UnicodeString; override;
+    procedure Decode(const AValue: UnicodeString); override;
+  end;
   // TQJsonSortFlags=(jsmAsString,);
   /// <summary>
   /// TQJson用于解析并维护JSON格式的对象类型，要使用前，需要先在堆中创建对应的实例。
@@ -8895,6 +8934,72 @@ begin
     FLocker.Leave;
   end;
 
+end;
+
+{ TBaseStreamCodec }
+
+constructor TBaseStreamCodec.Create;
+begin
+  inherited Create;
+end;
+
+destructor TBaseStreamCodec.Destroy;
+begin
+  if Assigned(FStream) then
+    FreeAndNil(FStream);
+  inherited;
+end;
+
+function TBaseStreamCodec.GetAsStream: TStream;
+begin
+  if not Assigned(FStream) then
+    FStream := TMemoryStream.Create;
+  Result := FStream;
+end;
+
+procedure TBaseStreamCodec.LoadFromBuffer(const ABytes: TBytes; const AOffset, ACount: UInt64);
+begin
+  GetAsStream.WriteBuffer(ABytes, AOffset, ACount);
+end;
+
+procedure TBaseStreamCodec.LoadFromBuffer(const p: PByte; ACount: UInt64);
+begin
+  GetAsStream.WriteBuffer(p^, ACount);
+end;
+
+procedure TBaseStreamCodec.LoadFromFile(const AFileName: UnicodeString; AMode: Word);
+begin
+  if Assigned(FStream) then
+    FreeAndNil(FStream);
+  FStream := TFileStream.Create(AFileName, AMode)
+end;
+
+{ TBase64StreamCodec }
+
+procedure TBase64StreamCodec.Decode(const AValue: UnicodeString);
+var
+  ABytes: TBytes;
+begin
+  ABytes := TNetEncoding.Base64.DecodeStringToBytes(AValue);
+  with GetAsStream do
+  begin
+    Size := Length(ABytes);
+    Position := 0;
+    WriteBuffer(ABytes[0], Length(ABytes));
+  end;
+end;
+
+function TBase64StreamCodec.Encode: UnicodeString;
+var
+  ABytes: TBytes;
+begin
+  with GetAsStream do
+  begin
+    SetLength(ABytes, Size);
+    Position := 0;
+    ReadBuffer(ABytes, Length(ABytes));
+    Result := TNetEncoding.Base64.EncodeBytesToString(ABytes);
+  end;
 end;
 
 initialization
